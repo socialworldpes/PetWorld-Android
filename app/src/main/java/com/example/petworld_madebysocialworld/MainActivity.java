1a1210ad1a1a1c2e11d2ddc8b0fb4067d795b893
RobertoUroz <44912234+RobertoUroz@users.noreply.github.com>
2019-03-20 23:55:44 +0100

    Primer push proyecto nuevo?

---
+++ app/src/main/java/com/example/petworld_madebysocialworld/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.petworld_madebysocialworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseApp f = FirebaseApp.initializeApp(this);
+        Object mAuth = FirebaseAuth.getInstance();
+        Log.d("LLEGA", "onCreate se ejecuta");
+        initButtons();
+        initListeners();
+        //initializeFireBase();
+        //loginFireBase();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //mapa mauri
+        /* val mapFragment = supportFragmentManager
+                 .findFragmentById(R.id.map) as SupportMapFragment
+         mapFragment.getMapAsync(this)*/
+    }
+
+    private void initListeners() {
+    }
+
+    private void initButtons() {
+    }
+}
