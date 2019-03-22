ab595a1d97d49ddf8c404d5de659940a5666e32b
¨Eric <emartinobispo@gmail.com>
2019-03-22 17:30:49 +0100

    log in working

--- app/src/main/java/com/example/petworld_madebysocialworld/MapActivity.java
+++ app/src/main/java/com/example/petworld_madebysocialworld/MapActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import com.google.android.gms.common.SignInButton;
 
 
 public class MapActivity extends AppCompatActivity {
