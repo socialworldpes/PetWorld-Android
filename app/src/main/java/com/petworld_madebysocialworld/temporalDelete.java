fb40b0a57698a819bb82da35015e19e844901464
rafa guardeno <rafa.guardeno.marquez@gmail.com>
2019-05-07 11:38:02 +0200

    delete routes, walks

--- app/src/main/java/com/petworld_madebysocialworld/temporalDelete.java
+++ app/src/main/java/com/petworld_madebysocialworld/temporalDelete.java
@@ -14,15 +14,15 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
 
 public class temporalDelete {
-    String userID;
-    FirebaseFirestore db;
+    private String userID;
+    private FirebaseFirestore db;
 
     public temporalDelete() {
         userID = User.getInstance().getAccount().getId();
         db = FirebaseFirestore.getInstance();
     }
 
-    public void borrarMeeting(final String meetingPath) {
+    public void deleteMeeting(final String meetingPath) {
 
         db.collection("users").document(userID).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -33,8 +33,62 @@ public class temporalDelete {
                         ArrayList<DocumentReference> alMeetingRef = (ArrayList<DocumentReference>) document.get("meetings");
                         for (DocumentReference dr : alMeetingRef) {
                             if (dr.getPath().equals(meetingPath)) {
-                                dr.delete(); //borra en meetings/
-                                document.getReference().update("pets", FieldValue.arrayRemove(dr)); //borra en users/meetings
+                                //borra en meetings/
+                                dr.delete();
+                                //borra referencia en users/meetings
+                                document.getReference().update("meetings", FieldValue.arrayRemove(dr));
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        });
+    }
+
+    public void deleteRoutes(final String routePath) {
+
+        db.collection("users").document(userID).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        ArrayList<DocumentReference> alMeetingRef = (ArrayList<DocumentReference>) document.get("routes");
+                        for (DocumentReference dr : alMeetingRef) {
+                            if (dr.getPath().equals(routePath)) {
+                                //borra en routes/
+                                dr.delete();
+                                //borra referencia en users/routes
+                                document.getReference().update("routes", FieldValue.arrayRemove(dr));
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        });
+    }
+
+    public void deleteWalks(final String walkPath) {
+
+        db.collection("users").document(userID).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        ArrayList<DocumentReference> alWalkRef = (ArrayList<DocumentReference>) document.get("walks");
+                        for (DocumentReference dr : alWalkRef) {
+                            if (dr.getPath().equals(walkPath)) {
+                                //borra en walks/
+                                dr.delete();
+                                //borra referencia en users/walks
+                                document.getReference().update("walks", FieldValue.arrayRemove(dr));
                                 break;
                             }
                         }
