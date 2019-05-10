94aa342458e68bd51ce23c52fad333499c785acb
rafa guardeno <rafa.guardeno.marquez@gmail.com>
2019-05-10 11:20:28 +0200

    Preview images when upload image

--- app/src/main/java/com/petworld_madebysocialworld/CreateRouteActivity.java
+++ app/src/main/java/com/petworld_madebysocialworld/CreateRouteActivity.java
@@ -5,7 +5,9 @@ import android.app.assist.AssistStructure;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Debug;
 import android.support.annotation.NonNull;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -91,8 +93,24 @@ public class CreateRouteActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 loadImage();
+
             }
         });
+    }
+
+    private void refreshImageView() {
+        Log.d("refreshing image view:", "in function");
+
+
+
+        for (Uri uri: uriImages)
+            urlImages.add(uri.toString());
+
+        ViewPager viewPager= findViewById(R.id.viewPager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getApplicationContext(), urlImages);
+        viewPager.setAdapter(adapter);
+
+
     }
 
     private void initFireBase() {
@@ -107,12 +125,14 @@ public class CreateRouteActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent imageData) {
         super.onActivityResult(requestCode, resultCode, imageData);
+        Log.d("onActivityresult", "in ");
         switch (requestCode) {
             case Define.ALBUM_REQUEST_CODE:
                 if (resultCode == RESULT_OK) {
                     uriImages = imageData.getParcelableArrayListExtra(Define.INTENT_PATH);
                     if (uriImages.size() > 0){
                         imagesCanContinue = true;
+                        refreshImageView();
                     }
                     break;
                 }
