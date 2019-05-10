9e08371e31040bdcdf655d1fc97973b76a8d367d
RobertoUroz <roberto.uroz@est.fib.upc.edu>
2019-05-10 17:40:33 +0200

    init friends fixed

--- app/src/main/java/com/petworld_madebysocialworld/ui/main/PlaceholderFragment.java
+++ app/src/main/java/com/petworld_madebysocialworld/ui/main/PlaceholderFragment.java
@@ -44,12 +44,12 @@ public class PlaceholderFragment extends Fragment {
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        View root = inflater.inflate(R.layout.activity_main, container, false);
         final TextView textView = root.findViewById(R.id.section_label);
         pageViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
             }
         });
         return root;
