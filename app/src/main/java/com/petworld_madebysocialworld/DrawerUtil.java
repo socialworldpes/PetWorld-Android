76874e031c607b25b7e05d65baed19e563f372ce
RobertoUroz <44912234+RobertoUroz@users.noreply.github.com>
2019-05-15 13:05:54 +0200

    Connection of 2 friends (Not tested) + DrawerUtil as Develop

--- app/src/main/java/com/petworld_madebysocialworld/DrawerUtil.java
+++ app/src/main/java/com/petworld_madebysocialworld/DrawerUtil.java
@@ -14,6 +14,8 @@ import android.widget.ImageView;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.*;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
@@ -40,20 +42,13 @@ public class DrawerUtil {
 
         i = 0;
         final HashMap<Integer, DocumentReference> mapPetRef =  new HashMap<>();
-        GoogleSignInAccount account = User.getInstance().getAccount();
 
-        String personName = "Name";
-        String personEmail = "Email";
-        Uri personPhoto = Uri.parse("http://www.example.com");
-
-        if (account == null) Log.d("PRUEBA==", "hola, no hay nadie en la account");
-        else{
-            //info account
-            personName = account.getDisplayName();
-            personEmail = account.getEmail();
-            personPhoto = account.getPhotoUrl();
-            Log.d("Prueba", personPhoto.getPath());
-        }
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        //info account
+        String personName = user.getDisplayName();
+        String personEmail = user.getEmail();
+        Uri personPhoto = user.getPhotoUrl();
+        String userID = user.getUid();
 
 
         DrawerImageLoader.init(new AbstractDrawerImageLoader() {
@@ -92,7 +87,6 @@ public class DrawerUtil {
                 .withIdentifier(2).withName("Mascotas").withIcon(R.drawable.ic_pets).withSelectable(false);
         //pets menu lateral
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        String userID = User.getInstance().getAccount().getId();
         DocumentReference docRef = db.collection("users").document(userID);
         Log.d("test", docRef.toString());
         Log.d("userID", userID);
@@ -133,9 +127,7 @@ public class DrawerUtil {
                         drawerItemManagePets.withSubItems(
                                 new SecondaryDrawerItem().withName("" + aux.get("name")).withLevel(2).withIdentifier(2001 + i)
                         );
-
                         i++;
-
                     }
                     */
                 }
@@ -215,12 +207,10 @@ public class DrawerUtil {
                             Intent intent = new Intent(activity, CreatePetActivity.class);
                             view.getContext().startActivity(intent);
                         }
-
                         if (drawerItem.getIdentifier() == 4 && !(activity instanceof FriendsActivity)){
                             Intent intent = new Intent(activity, FriendsActivity.class);
                             view.getContext().startActivity(intent);
                         }
-
                         return true;
                     }
                 })
@@ -229,4 +219,3 @@ public class DrawerUtil {
 
 
 }
-
--- app/src/main/java/com/petworld_madebysocialworld/FriendsActivity.java
+++ app/src/main/java/com/petworld_madebysocialworld/FriendsActivity.java
@@ -1,5 +1,7 @@
 package com.petworld_madebysocialworld;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,6 +12,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.petworld_madebysocialworld.ui.main.SectionsPagerAdapter;
 
 import java.util.ArrayList;
@@ -18,10 +25,12 @@ import java.util.Map;
 
 public class FriendsActivity extends AppCompatActivity {
 
+    private Activity activity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+        activity = this;
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
@@ -32,9 +41,16 @@ public class FriendsActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(activity, SearchFriendsActivity.class);
+                view.getContext().startActivity(intent);
             }
         });
+    }
+
+    //WHEN IT ACCEPTS THE PETITION, THIS FUNCTION DOES THE JOB OF CONNECTING
+    private void connect2Friends (String idUser, String idFriend){
+        //TODO TESTING IF IT REALLY ADDS FRIENDS OR JUST OVERWRITES
+        FirebaseFirestore.getInstance().collection("users").document(idUser).update("friends", FirebaseFirestore.getInstance().collection("users").document(idFriend));
+        FirebaseFirestore.getInstance().collection("users").document(idFriend).update("friends", FirebaseFirestore.getInstance().collection("users").document(idUser));
     }
 }
