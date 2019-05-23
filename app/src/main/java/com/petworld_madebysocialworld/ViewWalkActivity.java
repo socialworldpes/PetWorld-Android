c49047c3e47861f62e4173614a0770aab21fce8e
rafa guardeno <rafa.guardeno.marquez@gmail.com>
2019-05-23 19:58:35 +0200

    walk view. //TODO: arreglar imagenes create walk, edit() y delete()

--- app/src/main/java/com/petworld_madebysocialworld/ViewWalkActivity.java
+++ app/src/main/java/com/petworld_madebysocialworld/ViewWalkActivity.java
@@ -1,12 +1,15 @@
 package com.petworld_madebysocialworld;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.model.Document;
 
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
@@ -27,6 +31,7 @@ public class ViewWalkActivity extends AppCompatActivity {
     //id's
     String idWalk;
     String idRoute;
+    DocumentReference routeDocumentReference;
 
     //walk info
     String name;
@@ -37,6 +42,11 @@ public class ViewWalkActivity extends AppCompatActivity {
     //layout
     EditText nameWalkEditText;
     EditText descriptionWalkEditText;
+    EditText nameRouteEditText;
+
+    //buttons
+    Button editButton;
+    Button deleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +54,8 @@ public class ViewWalkActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_walk);
         initVariables();
         initLayout();
+        initButtons();
+        initListeners();
         setupToolbar();
         initFireBase();
 
@@ -51,29 +63,47 @@ public class ViewWalkActivity extends AppCompatActivity {
 
     }
 
+    private void initListeners() {
+        nameRouteEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goRoute();
+            }
+        });
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editWalk();
+            }
+        });
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteWalk();
+            }
+        });
+    }
+
+    private void deleteWalk() {
+    }
+
+    private void editWalk() {
+    }
+
+    private void initButtons() {
+        editButton = findViewById(R.id.editButton);
+        deleteButton = findViewById(R.id.deleteButton);
+    }
+
     private void initLayout() {
         nameWalkEditText = findViewById(R.id.nameWalkEditText);
-        descriptionWalkEditText =findViewById(R.id.descrptionWalkEditText);
+        descriptionWalkEditText = findViewById(R.id.descrptionWalkEditText);
+        nameRouteEditText = findViewById(R.id.nameRoutekEditText);
     }
 
     private void initFireBase() {
         db = FirebaseFirestore.getInstance();
-        DocumentReference docRef = db.collection("walks").document(idWalk);
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot result = task.getResult();
-                    name = (String) result.get("name");
-                    description = (String) result.get("description");
 
-                    setLayoutText();
-                }
-                else {
-                    Log.w("task ko", "Error getting documents.", task.getException());
-                }
-            }
-        });
     }
 
     private void setLayoutText() {
@@ -93,7 +123,62 @@ public class ViewWalkActivity extends AppCompatActivity {
         });
     }
     private void readWalkInfo() {
+        DocumentReference docRef = db.collection("walks").document(idWalk);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot result = task.getResult();
+                    name = (String) result.get("name");
+                    description = (String) result.get("description");
+                    imageUrls = (ArrayList<String>)result.get("images");
+                    routeDocumentReference = (DocumentReference) result.get("route");
 
+
+                    //images
+                    setImages();
+                    setLayoutText();
+                    readRouteInfo();
+                }
+                else {
+                    Log.w("task ko", "Error getting documents.", task.getException());
+                }
+            }
+        });
+    }
+
+    private void readRouteInfo() {
+
+
+        routeDocumentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot result = task.getResult();
+                    nameRouteEditText.setText((String) result.get("name"));
+                    idRoute = result.getId();
+
+
+                }
+                else {
+                    Log.w("task ko", "Error getting documents.", task.getException());
+                }
+            }
+        });
+
+    }
+
+    private void goRoute() {
+        Log.d("goRoute : " ,  "in ");
+        Intent nextActivity = new Intent(this, ViewRouteActivity.class);
+        nextActivity.putExtra("id", idRoute);
+        startActivity(nextActivity);
+    }
+
+    private void setImages() {
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getApplicationContext(), imageUrls);
+        viewPager.setAdapter(adapter);
     }
 
     private void initVariables() {
